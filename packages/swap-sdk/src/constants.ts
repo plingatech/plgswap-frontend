73bcc6ba8c38e6292eaaa6b9f34a6d0dbec6ae53
git <eae966@gmail.com>
2023-08-19 19:57:13 +0330

    Orbitex Factory has been added.

--- packages/swap-sdk/src/constants.ts
+++ packages/swap-sdk/src/constants.ts
@@ -2,10 +2,8 @@ import { Percent } from '@pancakeswap/swap-sdk-core'
 import { ERC20Token } from './entities/token'
 
 export enum ChainId {
-  ETHEREUM = 1,
-  GOERLI = 5,
   BSC = 56,
-  BSC_TESTNET = 97,
+  NEXI = 4242,
 }
 
 export const ZERO_PERCENT = new Percent('0')
@@ -13,52 +11,33 @@ export const ONE_HUNDRED_PERCENT = new Percent('1')
 
 export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
 
-const FACTORY_ADDRESS_ETH = '0x1097053Fd2ea711dad45caCcc45EfF7548fCB362'
+export const FACTORY_ADDRESS_NEXI = '0x7F07627F1c9222b5FC441Ae3e779bf18E782e028'
 
 export const FACTORY_ADDRESS_MAP: Record<number, string> = {
-  [ChainId.ETHEREUM]: FACTORY_ADDRESS_ETH,
-  [ChainId.GOERLI]: FACTORY_ADDRESS_ETH,
   [ChainId.BSC]: FACTORY_ADDRESS,
-  [ChainId.BSC_TESTNET]: '0x6725f303b657a9451d8ba641348b6761a6cc7a17',
+  [ChainId.NEXI]: FACTORY_ADDRESS_NEXI,
 }
 export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
 
-const INIT_CODE_HASH_ETH = '0x57224589c67f3f30a6b0d7a1b54cf3153ab84563bc609ef41dfb34f8b2974d2d'
+export const INIT_CODE_HASH_NEXI = '0x6bd0199d31e4f6b30ec806eac2cc68fe3027d8f958cf89039f04382a58bbef05'
+
 export const INIT_CODE_HASH_MAP: Record<number, string> = {
-  [ChainId.ETHEREUM]: INIT_CODE_HASH_ETH,
-  [ChainId.GOERLI]: INIT_CODE_HASH_ETH,
   [ChainId.BSC]: INIT_CODE_HASH,
-  [ChainId.BSC_TESTNET]: '0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66',
+  [ChainId.NEXI]: INIT_CODE_HASH_NEXI,
 }
 
-export const WETH9 = {
-  [ChainId.ETHEREUM]: new ERC20Token(
-    ChainId.ETHEREUM,
-    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+export const WNWXI = {
+  [ChainId.NEXI]: new ERC20Token(
+    ChainId.NEXI,
+    '0xEC3ceC066E5b2331fCD0Eb7eE5A9B17F617A6efb',
     18,
-    'WETH',
-    'Wrapped Ether',
-    'https://weth.io'
-  ),
-  [ChainId.GOERLI]: new ERC20Token(
-    ChainId.GOERLI,
-    '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
-    18,
-    'WETH',
-    'Wrapped Ether',
-    'https://weth.io'
+    'NEXI',
+    'Wrapped NEXI',
+    'https://nexi.technology'
   ),
 }
 
 export const WBNB = {
-  [ChainId.ETHEREUM]: new ERC20Token(
-    ChainId.ETHEREUM,
-    '0x418D75f65a02b3D53B2418FB8E1fe493759c7605',
-    18,
-    'WBNB',
-    'Wrapped BNB',
-    'https://www.binance.org'
-  ),
   [ChainId.BSC]: new ERC20Token(
     ChainId.BSC,
     '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
@@ -67,21 +46,11 @@ export const WBNB = {
     'Wrapped BNB',
     'https://www.binance.org'
   ),
-  [ChainId.BSC_TESTNET]: new ERC20Token(
-    ChainId.BSC_TESTNET,
-    '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
-    18,
-    'WBNB',
-    'Wrapped BNB',
-    'https://www.binance.org'
-  ),
 }
 
 export const WNATIVE: Record<number, ERC20Token> = {
-  [ChainId.ETHEREUM]: WETH9[ChainId.ETHEREUM],
-  [ChainId.GOERLI]: WETH9[ChainId.GOERLI],
+  [ChainId.NEXI]: WNWXI[ChainId.NEXI],
   [ChainId.BSC]: WBNB[ChainId.BSC],
-  [ChainId.BSC_TESTNET]: WBNB[ChainId.BSC_TESTNET],
 }
 
 export const NATIVE: Record<
@@ -92,16 +61,14 @@ export const NATIVE: Record<
     decimals: number
   }
 > = {
-  [ChainId.ETHEREUM]: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  [ChainId.GOERLI]: { name: 'Goerli Ether', symbol: 'GOR', decimals: 18 },
   [ChainId.BSC]: {
     name: 'Binance Chain Native Token',
     symbol: 'BNB',
     decimals: 18,
   },
-  [ChainId.BSC_TESTNET]: {
-    name: 'Binance Chain Native Token',
-    symbol: 'tBNB',
+  [ChainId.NEXI]: {
+    name: 'NEXI Chain Native Token',
+    symbol: 'NEXI',
     decimals: 18,
   },
 }
