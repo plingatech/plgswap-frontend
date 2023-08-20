03531f6872459da74af9b194351ed53e9ace12a4
git <eae966@gmail.com>
2023-08-20 13:17:26 +0330

    Some updates

--- packages/multicall/index.ts
+++ packages/multicall/index.ts
@@ -7,11 +7,8 @@ import type { Multicallv2Typed, Multicallv3Typed } from '@pancakeswap/utils/abit
 import multicallAbi from './Multicall.json'
 
 export const multicallAddresses = {
-  1: '0xcA11bde05977b3631167028862bE2a173976CA11',
-  4: '0xcA11bde05977b3631167028862bE2a173976CA11',
-  5: '0xcA11bde05977b3631167028862bE2a173976CA11',
   56: '0xcA11bde05977b3631167028862bE2a173976CA11',
-  97: '0xcA11bde05977b3631167028862bE2a173976CA11',
+  4242: '0x63ab18745f5c831Dc77de26fF2F03CAC352a41CE',
 }
 
 export const getMulticallContract = (chainId: ChainId, provider: Provider) => {
